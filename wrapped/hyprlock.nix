a30aa564ee6a0588b2a5fd2d4da92395c1db16e0
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-08 15:53:51 -0400

    wrapped/hyprlock: update widget settings

--- wrapped/hyprlock.nix
+++ wrapped/hyprlock.nix
@@ -23,21 +23,21 @@
             monitor = "";
             size = "200, 50";
             outline_thickness = 0;
-            dots_size = 0.275;
-            dots_spacing = 0.15;
+            dots_size = 0.25;
+            dots_spacing = 0.25;
             dots_center = true;
             dots_rounding = -1;
             outer_color = "rgb(151515)";
             inner_color = "rgb(FFFFFF)";
             font_color = "rgb(10, 10, 10)";
-            fade_on_empty = true;
+            # fade_on_empty = false;
             fade_timeout = 500;
-            placeholder_text = "Input Password";
+            placeholder_text = "Password";
             hide_input = false;
             rounding = -1;
-            check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
-            fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
-            fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+            check_color = "rgb(b4befe)";
+            fail_color = "rgba(f38ba8)";
+            fail_text = "$FAIL ($ATTEMPTS)";
             capslock_color = -1;
             numlock_color = -1;
             bothlock_color = -1;
@@ -47,49 +47,47 @@
             halign = "center";
             valign = "center";
           };
-        };
-        extraConfig = ''
-          label {
-              monitor =
+          label = [
+            {
+              monitor = "";
               #clock
-              text = $TIME12
-              color = rgba(255, 255, 255, 1.0)
-              font_size = 60
-              font_family = Fira Semibold
-              position = 0, 100
-              halign = center
-              valign = bottom
-              shadow_passes = 5
-              shadow_size = 10
-          }
-
-          label {
-              monitor =
-              text = <span text_transform="capitalize">$USER</span>
-              color = rgba(255, 255, 255, 1.0)
-              font_size = 30
-              font_family = Fira Semibold
-              position = 0, -75
-              halign = center
-              valign = center
-              shadow_passes = 5
-              shadow_size = 10
-          }
-
-          image {
-              monitor =
-              path = ${self.avatar}
-              size = 280 # lesser side if not 1:1 ratio
-              rounding = -1 # negative values mean circle
-              border_size = 0
-              border_color = rgb(221, 221, 221)
-              rotate = 0 
-              reload_time = -1 
-              position = 0, 100
-              halign = center
-              valign = center
-          }
-        '';
+              text = "$TIME12";
+              color = "rgba(255, 255, 255, 1.0)";
+              font_size = 82;
+              font_family = "Fira Semibold";
+              position = "0, 350px";
+              halign = "center";
+              valign = "center";
+              shadow_passes = 5;
+              shadow_size = 10;
+            }
+            {
+              monitor = "";
+              text = ''<span text_transform="capitalize">$USER</span>'';
+              color = "rgba(255, 255, 255, 1.0)";
+              font_size = 30;
+              font_family = "Fira Semibold";
+              position = "0, -75";
+              halign = "center";
+              valign = "center";
+              shadow_passes = 5;
+              shadow_size = 10;
+            }
+          ];
+          image = {
+            monitor = "";
+            path = "${self.avatar}";
+            size = 240; # lesser side if not 1:1 ratio
+            rounding = -1; # negative values mean circle
+            border_size = 0;
+            border_color = "rgb(221, 221, 221)";
+            rotate = 0;
+            reload_time = -1;
+            position = "0, 100";
+            halign = "center";
+            valign = "center";
+          };
+        };
       };
     };
 }
