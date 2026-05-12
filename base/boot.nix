efbfb53d05c74d3244e2de3a1f82fe8ffb4ad3ea
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-12 16:12:34 -0400

    base/boot: remove dirty frag update

--- base/boot.nix
+++ base/boot.nix
@@ -32,16 +32,6 @@
           btrfs = true;
           ntfs = true;
         };
-        extraModprobeConfig = ''
-          install esp4 ${pkgs.coreutils}/bin/false
-          install esp6 ${pkgs.coreutils}/bin/false
-          install rxrpc ${pkgs.coreutils}/bin/false
-        '';
-        blacklistedKernelModules = [
-          "esp4"
-          "esp6"
-          "rxrpc"
-        ];
       };
     };
 
