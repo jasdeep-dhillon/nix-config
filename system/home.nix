725c90808b6d9123d893e76c1271e32be33e6eba
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-16 06:23:26 -0400

    system/home: remove home-configuration

--- system/home.nix
+++ system/home.nix
@@ -1,8 +1,4 @@
-{
-  self,
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 {
   flake.nixosModules.home =
     { ... }:
@@ -15,27 +11,4 @@
         users.arc.home.stateVersion = "26.05";
       };
     };
-
-  flake.homeConfigurations.arc = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-    modules = with self.homeModules; [
-      base
-      theme
-      services
-      packages
-      programs
-
-      shortcuts
-
-      # Dev
-      # android
-      agents
-      editor
-      devApps
-      vcs
-      toolchain
-      zed
-      lsp
-    ];
-  };
 }
