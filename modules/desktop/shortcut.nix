e9043ea224a1b1dacfd5f45d3c5b6e6c9b6843d4
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-21 12:50:03 -0400

    modules/shortcut: move location and add helium incognito entry

---
+++ modules/desktop/shortcut.nix
@@ -0,0 +1,46 @@
+{ inputs, ... }:
+{
+  flake.homeModules.shortcuts =
+    { pkgs, lib, ... }:
+    {
+      xdg.desktopEntries = {
+        "windows" = {
+          name = "Windows";
+          categories = [ "Utility" ];
+          comment = "Boot to Windows";
+          exec = ''
+            pkexec ${(lib.getExe pkgs.nushell)} -c "bootctl set-oneshot auto-windows; reboot"
+          '';
+          icon = ../../icons/windows.ico;
+        };
+        "warp-connect" = {
+          name = "Warp Connect";
+          categories = [ "Utility" ];
+          comment = "Connect to Cloudflare Warp";
+          exec = "${pkgs.networkmanager}/bin/nmcli connect up Warp";
+          icon = ../../icons/openvpn.png;
+        };
+        "warp-disconnect" = {
+          name = "Warp Disconnect";
+          categories = [ "Utility" ];
+          comment = "Disconnect from Cloudflare Warp";
+          exec = "${pkgs.networkmanager}/bin/nmcli connect down Warp";
+          icon = ../../icons/openvpn.png;
+        };
+        "helium-incognito" = {
+          name = "Helium Incognito";
+          exec = "${
+            lib.getExe inputs.helium.packages.${pkgs.stdenv.hostPlatform.system}.default
+          } --incognito";
+          genericName = "Web Browser";
+          icon = ../../icons/helium.png;
+          terminal = false;
+          type = "Application";
+          categories = [
+            "Network"
+            "WebBrowser"
+          ];
+        };
+      };
+    };
+}
