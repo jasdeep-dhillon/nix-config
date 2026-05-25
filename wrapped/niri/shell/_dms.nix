8a670fd831b72bf9db947822c1f6e4284f254893
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-05-25 11:49:33 -0400

    combine all dms modules

--- wrapped/niri/shell/_dms.nix
+++ wrapped/niri/shell/_dms.nix
@@ -1,6 +1,6 @@
 { inputs, self, ... }:
 {
-  flake.nixosModules.dms = {
+  flake.nixosModules.niri = {
     imports = [
       inputs.home-manager.nixosModules.default
     ];
@@ -8,6 +8,269 @@
       imports = [ self.homeModules.dms ];
     };
   };
+  flake.nixosModules.niriShell =
+    { pkgs, lib, ... }:
+    let
+      dms-exe = lib.getExe inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    in
+    {
+      runtimePkgs = [ inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+      settings.environment = {
+        DMS_DISABLE_MATUGEN = "1";
+        DMS_DISABLE_CAVA = "1";
+      };
+      settings.spawn-at-startup = [
+        [
+          dms-exe
+          "run"
+        ]
+      ];
+
+      settings.switch-events = {
+        lid-close = {
+          spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "toast"
+            "info"
+            "Laptop Closed"
+          ];
+        };
+        lid-open = {
+          spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "toast"
+            "info"
+            "Laptop Opened"
+          ];
+        };
+      };
+      settings.binds = {
+        "Mod+F5" = {
+          spawn = [
+            dms-exe
+            "run"
+          ];
+        };
+        "Mod+Space" = _: {
+          props.cooldown-ms = 200;
+          props.hotkey-overlay-title = "Launcher";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "spotlight"
+            "toggle"
+          ];
+        };
+        "Mod+V" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Clipboard";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "clipboard"
+            "toggle"
+          ];
+        };
+        "Mod+Ctrl+Space" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Clipboard";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "spotlight"
+            "openQuery"
+            "> "
+          ];
+        };
+        "Mod+Period" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Emoji Picker";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "spotlight"
+            "openQuery"
+            ":e "
+          ];
+        };
+        "Mod+F3" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Output Switch";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "audio"
+            "cycleoutput"
+          ];
+        };
+        "Mod+I" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Settings";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "settings"
+            "toggle"
+          ];
+        };
+        "Mod+F1" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Lock Screen";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "lock"
+            "lock"
+          ];
+        };
+        "Mod+Escape" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Power Menu";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "powermenu"
+            "toggle"
+          ];
+        };
+        "Mod+A" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Control Center";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "control-center"
+            "toggle"
+          ];
+        };
+        "Mod+N" = _: {
+          props.repeat = false;
+          props.hotkey-overlay-title = "Notifications";
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "notifications"
+            "toggle"
+          ];
+        };
+
+        XF86MonBrightnessUp = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "brightness"
+            "increment"
+            "2"
+            ''""''
+          ];
+        };
+        "Mod+XF86AudioRaiseVolume" = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "brightness"
+            "increment"
+            "2"
+            ''""''
+          ];
+        };
+        "Mod+XF86AudioLowerVolume" = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "brightness"
+            "decrement"
+            "2"
+            ''""''
+          ];
+        };
+        XF86MonBrightnessDown = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "brightness"
+            "decrement"
+            "2"
+            ''""''
+          ];
+        };
+        XF86AudioPlay = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "mpris"
+            "playPause"
+          ];
+        };
+        XF86AudioStop = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "mpris"
+            "stop"
+          ];
+        };
+        XF86AudioPrev = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "mpris"
+            "previous"
+          ];
+        };
+        XF86AudioNext = _: {
+          props.allow-when-locked = true;
+          content.spawn = [
+            dms-exe
+            "ipc"
+            "call"
+            "mpris"
+            "next"
+          ];
+        };
+      };
+      settings.layer-rules = [
+        {
+          # matches = [
+          #   {
+          #     layer = "overlay";
+          #   }
+          # ];
+          # background-effect = {
+          #   blur = true;
+          #   xray = true;
+          # };
+        }
+      ];
+    };
 
   flake.homeModules.dms =
     { config, pkgs, ... }:
@@ -60,7 +323,7 @@
         inputs.dms.homeModules.dank-material-shell
         inputs.dms-plugin-registry.modules.default
       ];
-      home.file.".face".source = ../../avatar.jpg;
+      home.file.".face".source = self.avatar;
       programs.dank-material-shell = {
         enable = true;
         package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
