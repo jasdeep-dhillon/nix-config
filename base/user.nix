3813aee75c6e13aba05b77e49ff40ef5e6188e3a
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-06-05 00:44:12 -0400

    base/user: add public ssh key

--- base/user.nix
+++ base/user.nix
@@ -16,6 +16,9 @@
           "kvm"
         ];
         shell = pkgs.nushell;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMF3MhprJoOz8caTgMGGLq45EyXLF48RzAtwN1UJadL+"
+        ];
       };
     };
 }
