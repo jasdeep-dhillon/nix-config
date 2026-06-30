f16c2e9dc23ddc8dd4bd5bf0c3e8fb22a9689175
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-06-30 05:45:35 -0400

    system/lain/networking: map manually ran fluxer server to base url

--- system/lain/networking.nix
+++ system/lain/networking.nix
@@ -10,7 +10,9 @@
         allowedTCPPorts = [
           80
           443
+          7881
         ];
+        allowedUDPPorts = [ 7882 ];
       };
       services.tailscale.permitCertUid = lib.mkIf config.services.caddy.enable config.services.caddy.user;
       services.tailscale.useRoutingFeatures = "client";
@@ -30,6 +32,23 @@
             }
             reverse_proxy 127.0.0.1:8080
           '';
+
+          "fluxer.lain.home".extraConfig = ''
+            tls internal {
+              on_demand
+            }
+            reverse_proxy 127.0.0.1:8092
+          '';
+
+          "lain.tailc113f2.ts.net".extraConfig = ''
+            tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
+            reverse_proxy 127.0.0.1:8092
+          '';
+
+          # "qbit.lain.tailc113f2.ts.net".extraConfig = ''
+          #   tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
+          #   reverse_proxy 127.0.0.1:8080
+          # '';
         };
       };
     };
