14206bbcd1546b4381b3b8868c8f3734f6e49483
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-07-06 23:18:48 -0400

    system/lain/networking: move caddy to port 8443
    system/lain/networking: caddy config change

--- system/lain/networking.nix
+++ system/lain/networking.nix
@@ -16,6 +16,7 @@
           80
           443
           7881
+          8443
         ];
         allowedUDPPorts = [ 7882 ];
       };
@@ -23,37 +24,21 @@
       services.tailscale.useRoutingFeatures = "client";
       services.caddy = {
         enable = true;
+        httpsPort = 8443;
         virtualHosts = {
-          "media.lain.home".extraConfig = ''
+          "media.lain".extraConfig = ''
             tls internal {
               on_demand
             }
             reverse_proxy 127.0.0.1:8096
           '';
 
-          "qbit.lain.home".extraConfig = ''
+          "qbit.lain".extraConfig = ''
             tls internal {
               on_demand
             }
             reverse_proxy 127.0.0.1:8080
           '';
-
-          # "fluxer.lain.home".extraConfig = ''
-          #   tls internal {
-          #     on_demand
-          #   }
-          #   reverse_proxy 127.0.0.1:8092
-          # '';
-
-          # "lain.tailc113f2.ts.net".extraConfig = ''
-          #   tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
-          #   reverse_proxy 127.0.0.1:8092
-          # '';
-
-          # "qbit.lain.tailc113f2.ts.net".extraConfig = ''
-          #   tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
-          #   reverse_proxy 127.0.0.1:8080
-          # '';
         };
       };
 
