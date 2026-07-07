d5c8a11cf007be4afe369b1b2abc9b150c285e40
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-07-06 20:42:00 -0400

    system/lain/networking: open tailscale funnel automatically

--- system/lain/networking.nix
+++ system/lain/networking.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   flake.nixosModules.lain =
-    { config, lib, ... }:
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
     {
       networking.networkmanager.enable = true;
       networking.hostName = "lain";
@@ -33,12 +38,12 @@
             reverse_proxy 127.0.0.1:8080
           '';
 
-          "fluxer.lain.home".extraConfig = ''
-            tls internal {
-              on_demand
-            }
-            reverse_proxy 127.0.0.1:8092
-          '';
+          # "fluxer.lain.home".extraConfig = ''
+          #   tls internal {
+          #     on_demand
+          #   }
+          #   reverse_proxy 127.0.0.1:8092
+          # '';
 
           # "lain.tailc113f2.ts.net".extraConfig = ''
           #   tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
@@ -49,6 +54,26 @@
           #   tls /var/lib/tailscale/certs/lain.tailc113f2.ts.net.crt /var/lib/tailscale/certs/lain.tailc113f2.ts.net.key
           #   reverse_proxy 127.0.0.1:8080
           # '';
+        };
+      };
+
+      systemd.services.tailscale-funnel = {
+        description = "Tailscale Funnel to host fluxer server";
+        after = [
+          "tailscaled.service"
+          "network-online.target"
+          "podman-fluxer-caddy.service"
+        ];
+        wants = [
+          "tailscaled.service"
+          "network-online.target"
+        ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = "${pkgs.tailscale}/bin/tailscale funnel --bg --https=443 8092";
+          ExecStop = "${pkgs.tailscale}/bin/tailscale funnel --https=443 off";
         };
       };
     };
