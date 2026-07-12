d01f925a70831196ee896445da3173fc987c1280
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-07-12 09:14:05 -0400

    flake: add cuda binary cache

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.garnix.io/"
+      "https://cache.nixos-cuda.org"
       "https://nix-community.cachix.org/"
       "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
