2023ea7b677e39fd3ddb32a1ce5ea713f4a3c9a4
Jasdeep-Dhillon <jasdeepsdhillon@proton.me>
2026-09-05 19:41:46 -0400

    modules/udev: try to fix online software access

--- modules/udev.nix
+++ modules/udev.nix
@@ -2,8 +2,8 @@
 {
   flake.nixosModules.udev = {
     services.udev.extraRules = ''
-      KERNEL=="hidraw*", ATTRS{idVendor}=="352d", ATTRS{idProduct}=="2383", TAG+="uaccess"
-      KERNEL=="hidraw*", ATTRS{idVendor}=="3554", ATTRS{idProduct}=="f58a", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="352d", ATTRS{idProduct}=="2383", MODE="0666"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3554", ATTRS{idProduct}=="f58a", MODE="0666"
       SUBSYSTEM=="usb", ATTR{idVendor}=="048d", ATTR{idProduct}=="c975", MODE="0666"
     '';
   };
